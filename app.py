790437e39ec042f6127480435cc260013cc16af2
YanGeng <403331475@qq.com>
2022-12-05 21:33:45 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,8 @@
+from newsapp import app,db
+
+if __name__ == '__main__':
+    """db.drop_all()
+    db.create_all()"""
+    """db.create_all()"""
+    # test.CreateOrder()
+    app.run(debug=True,host = "127.0.0.1",port = 4096)
